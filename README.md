3944f070ddc74004f4d12111afda96e8660c0b11
Adetunji <madetunj@splprhpc05.cm.cluster>
2020-04-17 11:48:05 -0500

    update run_toil.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # test_toil
+
+# to execute on St. Jude
+
+```bash
+git clone https://github.com/madetunj/test_toil.git
+cd test_toil
+hpcf_interactive
+bash run_toil.sh 
+```
--- run_toil.sh
+++ run_toil.sh
@@ -18,13 +18,8 @@ toil-cwl-runner --batchSystem=lsf \
 ./workflow/echo_cat_wf.cwl in.yml 1>log.out 2>log.err
 
 
-for NF in outdir/*; do 
-  GS=(gold_standard/$file)
-  cmp $NF $GS
-done
 for NF in $(ls -1 outdir/*); do 
     NF=${NF##*/};
-    echo "comparing $NF";
     GS=(gold_standard/$NF);
     cmp outdir/$NF $GS; 
 done
